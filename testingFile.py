705584e10898a7d9cd7700e27ce4374440738d24
Danial Malik <danialzmalik@gmail.com>
2023-02-08 16:57:31 -0500

    using self built binary tree in testingFile.py

--- testingFile.py
+++ testingFile.py
@@ -1,3 +1,5 @@
+import binaryTree
+
 def sum(a,b):
     sum = a+b
     return sum
@@ -6,3 +8,7 @@ a=1
 b=2
 c=sum(a,b)
 print(c)
+
+testNode = binaryTree.Node(5)
+binaryTree.insert(testNode, 2)
+binaryTree.inorder(testNode)
