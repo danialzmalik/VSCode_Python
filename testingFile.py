8b0b739dd8668164cb1bd023bcdab6b8589ed9b7
Danial Malik <danialzmalik@gmail.com>
2023-03-17 09:27:53 -0400

    updating testingFile.py

--- testingFile.py
+++ testingFile.py
@@ -1,14 +1,25 @@
-import binaryTree
+# import binaryTree
 
-def sum(a,b):
-    sum = a+b
-    return sum
+# def sum(a,b):
+#     sum = a+b
+#     return sum
 
-a=1
-b=2
-c=sum(a,b)
-print(c)
+# a=1
+# b=2
+# c=sum(a,b)
+# print(c)
 
-testNode = binaryTree.Node(5)
-binaryTree.insert(testNode, 2)
-binaryTree.inorder(testNode)
+# testNode = binaryTree.Node(5)
+# binaryTree.insert(testNode, 2)
+# binaryTree.inorder(testNode)
+
+stack = []
+stack.append("a")
+
+while stack:
+    print("hi")
+    stack.pop()
+    # if not stack:
+    #     break
+    # print(stack)
+    # stack.pop()
